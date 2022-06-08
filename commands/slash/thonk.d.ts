f1b537a7bcea36ba628d26aa7d1523bfaea14075
chunkyzacko9 <107074410+chunkyzacko9@users.noreply.github.com>
2022-06-08 08:09:55 +0300

    Slash command thonk

---
+++ commands/slash/thonk.d.ts
@@ -0,0 +1,13 @@
+import { BaseSlashSubCommand } from "../baseslash";
+
+export declare class PxlThonkifySlashSubCommand extends BaseSlashSubCommand {
+
+    name: string;
+
+    description: string;
+
+    constructor();
+
+    run: typeof import("../../../../tools/format/pxl").Pxl.thonkify;
+
+}
